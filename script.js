6c025af4c9d29ac7a4e11da3cf2aa2f551381f43
César Muñoz <munozroldanc@gmail.com>
2018-09-01 09:33:25 -0500

    Se agregan servicios

--- script.js
+++ script.js
@@ -3,19 +3,62 @@ let taskApp = new Vue({
   data: {
     tasks: []
   },
+  mounted() {
+    this.$http.get("http://142.93.202.10:8080/tasks").then(
+      result => {
+        this.tasks = result.data;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  },
   methods: {
     deleteTask: function(task) {
-      this.tasks.splice(this.tasks.indexOf(task), 1);
+      //this.tasks.splice(this.tasks.indexOf(task), 1);
+      this.$http
+        .delete("http://142.93.202.10:8080/tasks", { body: { id: task.id } })
+        .then(
+          result => {
+            console.log(JSON.stringify(result.data));
+            //Borra la primera ocurrencia de task
+            this.tasks.splice(this.tasks.indexOf(task), 1);
+          },
+          error => {
+            console.error(error);
+          }
+        );
     },
     addTask: function(e) {
       e.preventDefault();
+      //This if prevents that the input adds empty tasks :D
       if (this.tasks.name) {
-        this.tasks.push({
+        let task = {
           name: this.tasks.name,
           done: false
-        });
+        };
+        this.$http.post("http://142.93.202.10:8080/tasks", task).then(
+          result => {
+            this.tasks.push(result.data);
+            console.log(this.tasks);
+          },
+          error => {
+            console.error(error);
+          }
+        );
         this.tasks.name = "";
       }
+    },
+    updateTask: function(task) {
+      this.$http.put("http://142.93.202.10:8080/tasks", task).then(
+        result => {
+          console.log(JSON.stringify(result.data));
+          this.tasks = result.body;
+        },
+        error => {
+          console.error(error);
+        }
+      );
     }
   }
 });
